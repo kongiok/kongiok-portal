00fb5dde53ba33e64f7c61eb29928f7c6d93de5b
kuang-yu-wang <211410146@o365.tku.edu.tw>
2024-04-09 17:40:36 +0800

    Feat: project initialize
    Learning Vue is quite fun though.

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <header>This is Header</header>
+    <slot />
+    <footer>This is Footer</footer>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+let AuthorName = "Ong Kong Giok";
+let AuthorTitle = "Software Engineer";
+let AuthorPicture = "/images/capybara_profile.png";
+</script>
+
+<template>
+  <div class="min-h-dvh min-w-dvw h-full w-full bg-surface">
+    <section class="hero">
+      <div class="hero--picture">
+        <img :src="AuthorPicture" :alt="`Picture of ${AuthorName}`">
+      </div>
+      <div class="hero--titles">
+        <h1>{{ AuthorName }}</h1>
+        <h2>{{ AuthorTitle }}</h2>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style>
+</style>
