e45ae9cea705162817201500f4dd69c8f4f24b44
kuang-yu-wang <211410146@o365.tku.edu.tw>
2024-05-04 11:08:09 +0800

    feat: create some types and add some components

--- lib/types.ts
+++ lib/types.ts
@@ -1,5 +1,19 @@
+import { IconType } from 'react-icons';
 export type LinkType = {
   href: string;
   label: string;
-  icon?: string | SVGElement;
+  icon?: string | IconType;
+  classes?: string;
+}
+
+export type AlertArgs = {
+  type: AlertTypes;
+  children: React.ReactNode;
+};
+
+export enum AlertTypes {
+  SUCCESS = "success",
+  WARNING = "warning",
+  DANGER = "danger",
+  INFO = "info",
 }
